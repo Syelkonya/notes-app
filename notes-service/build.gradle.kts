459769f854a87b98bb778d2e4d6df1e56ba47552
Syelkonya <teldar07@gmail.com>
2026-07-05 17:12:49 +0300

    add eureka

--- notes-service/build.gradle.kts
+++ notes-service/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	compileOnly("org.projectlombok:lombok:1.18.46")
 	annotationProcessor("org.projectlombok:lombok:1.18.46")
 
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
 	implementation("com.opencsv:opencsv:5.12.0")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
@@ -31,6 +32,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+extra["springCloudVersion"] = "2025.1.2"
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
 }
 
 tasks.withType<Test> {
