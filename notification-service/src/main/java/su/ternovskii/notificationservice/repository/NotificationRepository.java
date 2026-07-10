1fdf4bb501b8939165b6f2298a8f6c63422d1ad2
Syelkonya <teldar07@gmail.com>
2026-07-10 20:24:43 +0300

    fix n+1

--- notification-service/src/main/java/su/ternovskii/notificationservice/repository/NotificationRepository.java
+++ notification-service/src/main/java/su/ternovskii/notificationservice/repository/NotificationRepository.java
@@ -1,10 +1,14 @@
 package su.ternovskii.notificationservice.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import su.ternovskii.notificationservice.entity.NotificationEntity;
 
+import java.util.List;
+
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
 
-
+    @EntityGraph(attributePaths = "deliveryAttempts")
+     List<NotificationEntity> findAll();
 
 }
